846304ecfbc953a5f87d3485bdfa101fd00ced7b
womackx <womackx@live.com>
2017-10-05 13:29:46 +0100

    mongo db CRUD functionality implemented

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -9,90 +9,39 @@ const app = express();
 const port = process.env.port || 3000;
 const router = express.Router();
 
+//Mongo
+const db = require("../config/db");
+const Dog = require("../controller/dog");
+
 //Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Routing
-app.use("/api/dog", router);
+app.use("/", router);
 
 /* GET REQUESTS */
 //Provide all dogs on base url of https://localhost:3000/api/dog
-router.get("/", (req, res) => {
-    res.json(dogs);
-});
+router.get("/api/dog", Dog.getAll);
 
 //Get a specific dog based on id provided 
 //such as /api/dog/2
-router.get("/:id", (req, res) => {
-    const dogID = req.params.id;
-    const currentDog = dogs.find((dog) => dog.id == dogID);
-    if (currentDog) { res.json(currentDog); }
-    else { res.sendStatus(404); }
-});
+router.get("/api/dog/:id", Dog.get);
 
 /* POST REQUESTS */
 //Base url, takes a dog with the four attributes, checks if its valid and unique, then adds it to the array.
-router.post("/", (req, res) => {
-    const postDog = req.body;
-    const isValid = isValidDog(postDog) && !dogs.find((a) => a.id == postDog.id);
-    if (isValid) {
-        dogs.push(postDog);
-        res.send(postDog);
-    }
-    else { res.sendStatus(500); }
-});
+router.post("/api/dog/", Dog.create);
 
 /* PUT REQUESTS */
 //Take a dog object at a specific ID url e.g. /api/dog/1
 //Replace contents of the dog with that id, with the dog recieved, not including the ID.
-router.put("/:id", (req, res) => {
-    const dogID = req.params.id;
-    const currentDog = dogs.find((dog) => dog.id == dogID);
-    if (currentDog) {
-        const putDog = req.body;
-        const isValid = isValidDog(putDog);
-        if (isValid) {
-            currentDog.name = putDog.name;
-            currentDog.age = putDog.age;
-            currentDog.species = putDog.species;
-            res.sendStatus(204);
-        }
-        else { res.sendStatus(404); }
-    }
-});
+router.put("/api/dog/:id", Dog.update);
 
 /* DELETE REQUESTS */
 //Doesn't take any information, a delete request at a specific ID endpoint will delete the object.
-router.delete("/:id", (req, res) => {
-    const dogID = req.params.id;
-    const currentDog = dogs.findIndex((dog) => dog.id == dogID);
-    if (currentDog !== -1) {
-        dogs.splice(currentDog, 1);
-        res.sendStatus(204);
-    }
-    else {
-        res.sendStatus(404);
-    }
-});
+router.delete("/api/dog/:id", Dog.delete);
 
-
-//Mock data instead of database
-const dogs = [
-    {
-        id: 1,
-        name: "Sir borks a lot",
-        age: 3,
-        species: "Pug"
-    }
-]
-
-//Checks if the object has all required attributes
-function isValidDog(dog) {
-    return dog.id && dog.name && dog.age && dog.species;
-}
-
-//Run server
+//Run server, open browser.
 app.listen(port, (err) => {
     if (err) { console.log(err); }
     else { open(`http://localhost:${port}`) }
